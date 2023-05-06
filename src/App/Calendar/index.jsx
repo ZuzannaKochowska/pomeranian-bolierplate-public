299a1c74f90afd25a8a623589a2adcddf3b3f28b
ZuzannaKochowska <zuzannakochowska@gmail.com>
2023-05-06 16:46:07 +0200

    New settings

---
+++ src/App/Calendar/index.jsx
@@ -0,0 +1,12 @@
+import './styles.css';
+
+export const calendar = () => {
+  return (
+  <div className= "dashboard" >
+    <h1>CV</h1>
+    <p>
+      Tutaj znajduje się moj kalendarz
+    </p>
+  </div>
+  );  
+};
---
+++ src/App/Settings/index.jsx
@@ -0,0 +1,12 @@
+import './styles.css';
+
+export const settings = () => {
+  return (
+  <div className= "dashboard" >
+    <h1>CV</h1>
+    <p>
+      Tutaj znajduja sie ustawienia
+    </p>
+  </div>
+  );  
+};
