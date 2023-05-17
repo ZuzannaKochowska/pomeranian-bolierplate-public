bb745a0faeba6a3d0deb988aeb8e6b51a16dfbdc
ZuzannaKochowska <zuzannakochowska@gmail.com>
2023-05-17 18:49:40 +0200

    Blog

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,6 @@
 import React from 'react';
 import { Routes, Route, BrowserRouter, Navigate } from 'react-router-dom';
-
 import { NotFound } from './App/Components/NotFound/NotFound';
-
 import { Blocks } from './App/Blocks';
 import { Layout } from './App/Layout';
 import { Dashboard } from './App/Dashboard';
@@ -10,6 +8,8 @@ import { CV } from './App/CV';
 import { Exercises } from './App/Exercises';
 import { Settings } from './App/Settings';
 import { Calendar } from './App/Calendar';
+import { Blog } from './App/Blog';
+
 
 export function App() {
   return (
@@ -18,10 +18,10 @@ export function App() {
         <Route path="" element={<Layout withSidebar />}>
           <Route index element={<Navigate to="dashboard" />} />
           <Route path="dashboard/*" element={<Dashboard />} />
-
           <Route path="cv/*" element={<CV />} />
           <Route path="exercises/*" element={<Exercises />} />
           <Route path="Calendar/*" element={<Calendar />} />
+          <Route path="Blog/*" element={<Blog />} />
           <Route path="Settings/*" element={<Settings />} />
           <Route path="blocks/*" element={<Blocks />} />
           <Route path="*" element={<NotFound />} />
---
+++ src/App/Blog/index.jsx
@@ -0,0 +1,12 @@
+import './styles.css';
+
+export const Blog = () => {
+  return (
+  <div className= "dashboard" >
+    <h1>Blog</h1>
+    <p>
+      Tu mój blog.
+    </p>
+  </div>
+  );  
+};
