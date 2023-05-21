3e0dcbfaf448d9395537e4ebc7eabca88cb3a8df
ZuzannaKochowska <zuzannakochowska@gmail.com>
2023-05-21 11:11:54 +0200

    cookies

---
+++ src/App/Components/CookiesBanner/router-data.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+import { CalendarIcon } from './CookiesBanner';
+
+export const blockRouterMetaData = {
+  path: '12-12-2023-2',
+  date: '12-12-2023',
+  linkLabel: 'Standardowe Tagi',
+  blockNo: 1,
+  element: <StandardTags />,
+  tags: ['html', 'css', 'styling'],
+};
