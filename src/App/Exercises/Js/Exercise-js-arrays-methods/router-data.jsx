153cf2b2e776c1a64e5a352c66dfa98015f05ab7
ZuzannaKochowska <zuzannakochowska@gmail.com>
2023-05-27 19:38:12 +0200

    methods

--- src/App/Exercises/Js/Exercise-js-arrays-methods/router-data.jsx
+++ src/App/Exercises/Js/Exercise-js-arrays-methods/router-data.jsx
@@ -1,10 +1,10 @@
-// import { ExerciseJsArraysMethods } from './index';
+import { ExerciseJsArraysMethods } from './index';
 
-// export const blockRouterMetaData = {
-//   path: 'Exercise-js-arrays-methods',
-//   date: null,
-//   linkLabel: 'Exercise-js-arrays-methods',
-//   blockNo: 20,
-//   element: <ExerciseJsArraysMethods />,
-//   tags: ['array', 'javascript'],
-// };
+export const blockRouterMetaData = {
+  path: 'Exercise-js-arrays-methods',
+  date: null,
+  linkLabel: 'Exercise-js-arrays-methods',
+  blockNo: 20,
+  element: <ExerciseJsArraysMethods />,
+  tags: ['array', 'method', 'javascript'],
+};
