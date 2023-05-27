6f4c1e4d3d0682c46e6f14aca11b5523ae0383c3
ZuzannaKochowska <zuzannakochowska@gmail.com>
2023-05-27 11:43:55 +0200

    remarks

--- src/App/Exercises/Js/Exercise-js-arrays-methods/index.jsx
+++ src/App/Exercises/Js/Exercise-js-arrays-methods/index.jsx
@@ -1,12 +1,12 @@
-import './styles.css';
+// import './styles.css';
 
-function getThisSameNumbers() {
-  const lista1 = [1, 2, 3, 4, 5];
-  const lista2 = [3, 5, 7, 9];
+// function getThisSameNumbers() {
+//   const lista1 = [1, 2, 3, 4, 5];
+//   const lista2 = [3, 5, 7, 9];
 
-  const result = arrOne.filter(function (numberElement) {
-    return arrTwo.includes(numberElement);
-  });
-  return console.log('getThisSameNumbers', result);
-}
-getThisSameNumbers();
+//   const result = arrOne.filter(function (numberElement) {
+//     return arrTwo.includes(numberElement);
+//   });
+//   return console.log('getThisSameNumbers', result);
+// }
+// getThisSameNumbers();
