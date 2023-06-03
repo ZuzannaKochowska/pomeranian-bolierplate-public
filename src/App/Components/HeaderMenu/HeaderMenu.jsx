b75ad2144776ca23d809638cf244c4aeed34f520
ZuzannaKochowska <zuzannakochowska@gmail.com>
2023-06-03 04:02:23 +0200

    Dashboard remarks

--- src/App/Components/HeaderMenu/HeaderMenu.jsx
+++ src/App/Components/HeaderMenu/HeaderMenu.jsx
@@ -7,17 +7,17 @@ import { ReactComponent as LogoSVG } from '../../Images/startItlogo.svg';
 export function HeaderMenu() {
   return (
     <header>
-      <a href="/">
+      {/* <a href="/">
         {''}
         <img src={LogoSVG} className="logo" alt="logo" />
-      </a>
+      </a> */}
       <div className="images-container">
         <SettingIcon />
         <div className="circular_image">
           <img src="https://placekitten.com/500/500" />
         </div>
         <div className="name-surname-container">
-          <h4>Zuzanna Kochowska</h4>
+          <h4>Zuzanna </h4>
           <h5>Kursantka </h5>
         </div>
 
