3c2f6588e2322f28b24ad45fafa70c395d59e0fb
ZuzannaKochowska <zuzannakochowska@gmail.com>
2023-06-23 12:39:05 +0200

    BasicForms

--- src/App/Exercises/ReactExercises/view-router-data.jsx
+++ src/App/Exercises/ReactExercises/view-router-data.jsx
@@ -3,13 +3,16 @@ import { getParsedRouterDataList } from '../../router-data/parseRouterData';
 import { SubRouteExampleMetaData } from './SubRouteExample/router-data';
 import { Blok8MetaData } from './Blok8/router-data';
 import { Blok8MetaData as FormsMetaData } from './Forms/router-data';
-
+// import { Blok8MetaData as BasicFormsMetaData } from './BasicForms/router-data';
+import { Blok8MetaData as BasicForms } from './BasicForms/router-data';
 // import { blockRouterMetaData as blockRouterMetaData12052023 } from '../Exercises/HtmlCss/Exercise-example-12-05-2023/router-data';
 
 export const blockRouterMetaData = [
   SubRouteExampleMetaData,
   Blok8MetaData,
   FormsMetaData,
+  // BasicFormsMetaData,
+  BasicForms,
 ];
 export const blockRouterData = getParsedRouterDataList(blockRouterMetaData);
 
