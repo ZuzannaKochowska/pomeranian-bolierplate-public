f122fe20144371eed44dcdba32c41acc50d489d0
ZuzannaKochowska <zuzannakochowska@gmail.com>
2023-07-26 22:54:09 +0200

    proba

--- src/App/Exercises/ReactExercises/view-router-data.jsx
+++ src/App/Exercises/ReactExercises/view-router-data.jsx
@@ -2,15 +2,15 @@ import { getParsedRouterDataList } from '../../router-data/parseRouterData';
 
 import { SubRouteExampleMetaData } from './SubRouteExample/router-data';
 import { Blok8MetaData } from './Blok8/router-data';
-import { Blok8MetaData as FormsMetaData } from './Forms/router-data';
-import { Blok8MetaData as BasicForms } from './BasicForms/router-data';
+// import { Blok8MetaData as FormsMetaData } from './Forms/router-data';
+// import { Blok8MetaData as BasicForms } from './BasicForms/router-data';
 // import { blockRouterMetaData as blockRouterMetaData12052023 } from '../Exercises/HtmlCss/Exercise-example-12-05-2023/router-data';
 import { SubRouteExampleMetaData as Redux } from './Redux/router-data';
 
 export const blockRouterMetaData = [
   SubRouteExampleMetaData,
   Blok8MetaData,
-  FormsMetaData,
+  // FormsMetaData,
 ];
 export const blockRouterData = getParsedRouterDataList(blockRouterMetaData);
 
