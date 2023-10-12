08b04eb83691af2316a6175dfb0302f109807ef5
ZuzannaKochowska <zuzannakochowska@gmail.com>
2023-10-12 20:29:42 +0200

    klasy

--- src/App/Exercises/HtmlCss/BoxModel/index.jsx
+++ src/App/Exercises/HtmlCss/BoxModel/index.jsx
@@ -8,7 +8,7 @@ export function BoxModel() {
   };
 
   return (
-    <div>
+    <div className="box-model-base">
       <p className="paragraph" onClick={clickHandler}>
         Lorem Ipsum is simply dummy text of the printing and typesetting
         industry. Lorem Ipsum has been the industry's standard dummy text ever
@@ -24,6 +24,18 @@ export function BoxModel() {
         {isTrue && <span className="inline-span"> Test inlineowy!</span>}
       </p>
       <p className="paragraph content-box">
+        Lorem Ipsum is simply dummy text of the printing and typesetting
+        industry. Lorem Ipsum has been the industry's standard dummy text ever
+        since the 1500s, when an unknown printer took a galley of type and
+        scrambled it to make a type specimen book. It has survived not only five
+        centuries, but also the leap into electronic typesetting, remaining
+        essentially unchanged. It was popularised in the 1960s with the release
+        of Letraset sheets containing Lorem Ipsum passages, and more recently
+        with desktop publishing software like Aldus PageMaker including versions
+        of Lorem Ipsum.
+        <span className="inline-span"> Test inlineowy!</span>
+      </p>
+      <p className="paragraph relative-box">
         Lorem Ipsum is simply dummy text of the printing and typesetting
         industry. Lorem Ipsum has been the industry's standard dummy text ever
         since the 1500s, when an unknown printer took a galley of type and
