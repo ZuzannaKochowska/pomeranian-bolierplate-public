c478ca36f5569070a40a8ac5ba3e8809595a8e0f
ZuzannaKochowska <zuzannakochowska@gmail.com>
2023-10-25 01:24:40 +0200

    conditional className

--- src/App/Components/FAQ/SingleQuestion.jsx
+++ src/App/Components/FAQ/SingleQuestion.jsx
@@ -6,21 +6,25 @@ export const SingleQuestion = (props) => {
   const question = props.question;
   const answer = props.answer;
 
-  const [isTrue, setIsTrue] = useState(false);
+  const [isActive, setIsActive] = useState(false);
 
   function clickHandler() {
-    setIsTrue(!isTrue);
+    setIsActive(!isActive);
   }
 
   return (
     <div>
       <div className="blok1">
         <h3 className="podtytulek" onClick={clickHandler}>
-          <img src={strzalka} alt="strzalka"></img>
+          <img
+            src={strzalka}
+            alt="strzalka"
+            className={` ${isActive ? 'rotate' : ''}`}
+          ></img>
           {question}
         </h3>
         <hr></hr>
-        <div>{isTrue && answer}</div>
+        <div>{isActive && answer}</div>
       </div>
     </div>
   );
